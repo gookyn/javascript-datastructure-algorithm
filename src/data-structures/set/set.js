c7d3065a52063b20d18b421a85b446b8388f78b1
gookyn <liujinge.work@gmail.com>
2022-12-05 20:20:14 +0800

    feat: set operation

--- src/data-structures/set/set.js
+++ src/data-structures/set/set.js
@@ -72,6 +72,73 @@ class Set {
 		}
 		return str;
 	}
+
+	// 并集
+	union(otherSet) {
+		const unionSet = new Set();
+		this.values().forEach(item => unionSet.add(item));
+		otherSet.values().forEach(item => unionSet.add(item));
+		return unionSet;
+	}
+
+	// 交集
+	intersection(otherSet) {
+		const intersectionSet = new Set();
+
+		let smallerSet = this;
+		let biggerSet = otherSet;
+		if (this.size() > otherSet.size()) {
+			smallerSet = otherSet;
+			biggerSet = this;
+		}
+
+		// 迭代长度较小的集合
+		smallerSet.values().forEach(item => {
+			if (biggerSet.has(item)) {
+				intersectionSet.add(item);
+			}
+		});
+
+		return intersectionSet;
+	}
+
+	// 差集
+	difference(otherSet) {
+		const differenceSet = new Set();
+		this.values().forEach(item => {
+			if (!otherSet.has(item)) {
+				differenceSet.add(item);
+			}
+		});
+		return differenceSet;
+	}
+
+	// 当前集合是否为另一个集合的子集
+	isSubsetOf(otherSet) {
+		if (this.size() > otherSet.size()) {
+			return false;
+		}
+
+		return this.values().every(item => otherSet.has(item));
+	}
+
+	// 使用 ES6 扩展运算符
+	// this 需要可迭代
+	// union(otherSet) {
+	// 	return new Set([...this, ...otherSet]);
+	// }
+
+	// intersection(otherSet) {
+	// 	return new Set([...this].filter(item => otherSet.has(item)));
+	// }
+
+	// difference(otherSet) {
+	// 	return new Set([...this].filter(item => !otherSet.has(item)));
+	// }
+
+	// isSubsetOf(otherSet) {
+	// 	return [...this].every(item => otherSet.has(item));
+	// }
 }
 
 module.exports = Set;
--- test/data-structures/set/set.spec.js
+++ test/data-structures/set/set.spec.js
@@ -147,4 +147,242 @@ describe('Set', () => {
 		set.add(new Obj(3, 4));
 		expect(set.toString()).to.equal('1:2,3:4');
 	});
+
+	/**
+	 * union
+	 */
+	function addValues(min, max) {
+		set = new Set();
+		for (let i = min; i <= max; i++) {
+			set.add(i);
+		}
+		return set;
+	}
+
+	it('union between empty sets', () => {
+		const set1 = new Set();
+		const set2 = new Set();
+
+		let resultSet = set1.union(set2);
+		expect(resultSet.isEmpty()).to.equal(true);
+
+		resultSet = set2.union(set1);
+		expect(resultSet.isEmpty()).to.equal(true);
+	});
+
+	it('union between equal sets', () => {
+		const set1 = addValues(1, 10);
+		const set2 = addValues(1, 10);
+
+		let resultSet = set1.union(set2);
+		for (let i = 1; i <= 10; i++) {
+			expect(resultSet.has(i)).to.equal(true);
+		}
+
+		resultSet = set2.union(set1);
+		for (let i = 1; i <= 10; i++) {
+			expect(resultSet.has(i)).to.equal(true);
+		}
+	});
+
+	it('union between different sets', () => {
+		const set1 = addValues(1, 5);
+		const set2 = addValues(6, 10);
+
+		let resultSet = set1.union(set2);
+		for (let i = 1; i <= 10; i++) {
+			expect(resultSet.has(i)).to.equal(true);
+		}
+
+		resultSet = set2.union(set1);
+		for (let i = 1; i <= 10; i++) {
+			expect(resultSet.has(i)).to.equal(true);
+		}
+	});
+
+	it('union between sets with common values', () => {
+		const set1 = addValues(1, 8);
+		const set2 = addValues(3, 10);
+
+		let resultSet = set1.union(set2);
+		for (let i = 1; i <= 10; i++) {
+			expect(resultSet.has(i)).to.equal(true);
+		}
+
+		resultSet = set2.union(set1);
+		for (let i = 1; i <= 10; i++) {
+			expect(resultSet.has(i)).to.equal(true);
+		}
+	});
+
+	/**
+	 * intersection
+	 */
+	it('intersection between empty sets', () => {
+		const set1 = new Set();
+		let set2 = new Set();
+
+		let resultSet = set1.intersection(set2);
+		expect(resultSet.isEmpty()).to.equal(true);
+
+		resultSet = set2.intersection(set1);
+		expect(resultSet.isEmpty()).to.equal(true);
+	});
+
+	it('intersection between equal sets', () => {
+		const set1 = addValues(1, 10);
+		const set2 = addValues(1, 10);
+
+		let resultSet = set1.intersection(set2);
+		for (let i = 1; i <= 10; i++) {
+			expect(resultSet.has(i)).to.equal(true);
+		}
+
+		resultSet = set2.intersection(set1);
+		for (let i = 1; i <= 10; i++) {
+			expect(resultSet.has(i)).to.equal(true);
+		}
+	});
+
+	it('intersection between different sets', () => {
+		const set1 = addValues(1, 5);
+		const set2 = addValues(6, 10);
+
+		let resultSet = set1.intersection(set2);
+		expect(resultSet.isEmpty()).to.equal(true);
+
+		resultSet = set2.intersection(set1);
+		expect(resultSet.isEmpty()).to.equal(true);
+	});
+
+	it('intersection between sets with common values', () => {
+		const set1 = addValues(1, 8);
+		const set2 = addValues(3, 10);
+
+		let resultSet = set1.intersection(set2);
+		for (let i = 3; i <= 8; i++) {
+			expect(resultSet.has(i)).to.equal(true);
+		}
+
+		resultSet = set2.intersection(set1);
+		for (let i = 3; i <= 8; i++) {
+			expect(resultSet.has(i)).to.equal(true);
+		}
+	});
+
+	/**
+	 * difference
+	 */
+	it('difference between empty sets', () => {
+		const set1 = new Set();
+		let set2 = new Set();
+
+		let resultSet = set1.union(set2);
+		expect(resultSet.isEmpty()).to.equal(true);
+
+		resultSet = set2.union(set1);
+		expect(resultSet.isEmpty()).to.equal(true);
+	});
+
+	it('difference between equal sets', () => {
+		const set1 = addValues(1, 10);
+		const set2 = addValues(1, 10);
+
+		let resultSet = set1.difference(set2);
+		for (let i = 1; i <= 10; i++) {
+			expect(resultSet.has(i)).to.equal(false);
+		}
+
+		resultSet = set2.difference(set1);
+		for (let i = 1; i <= 10; i++) {
+			expect(resultSet.has(i)).to.equal(false);
+		}
+	});
+
+	it('difference between different sets', () => {
+		const set1 = addValues(1, 5);
+		const set2 = addValues(6, 10);
+
+		let resultSet = set1.difference(set2);
+		for (let i = 1; i <= 10; i++) {
+			if (i <= 5) {
+				expect(resultSet.has(i)).to.equal(true);
+			} else {
+				expect(resultSet.has(i)).to.equal(false);
+			}
+		}
+
+		resultSet = set2.difference(set1);
+		for (let i = 1; i <= 10; i++) {
+			if (i <= 5) {
+				expect(resultSet.has(i)).to.equal(false);
+			} else {
+				expect(resultSet.has(i)).to.equal(true);
+			}
+		}
+	});
+
+	it('difference between sets with common values', () => {
+		const set1 = addValues(1, 8);
+		const set2 = addValues(3, 10);
+
+		let resultSet = set1.difference(set2);
+		for (let i = 1; i <= 10; i++) {
+			if (i < 3) {
+				expect(resultSet.has(i)).to.equal(true);
+			} else {
+				expect(resultSet.has(i)).to.equal(false);
+			}
+		}
+
+		resultSet = set2.difference(set1);
+		for (let i = 1; i <= 10; i++) {
+			if (i < 3) {
+				expect(resultSet.has(i)).to.equal(false);
+			} else if (i > 8) {
+				expect(resultSet.has(i)).to.equal(true);
+			}
+		}
+	});
+
+	/**
+	 * isSubsetOf
+	 */
+	it('isSubsetOf between empty sets', () => {
+		const set1 = new Set();
+		let set2 = new Set();
+
+		expect(set1.isSubsetOf(set2)).to.equal(true);
+		expect(set2.isSubsetOf(set1)).to.equal(true);
+	});
+
+	it('isSubsetOf between equal sets', () => {
+		const set1 = addValues(1, 10);
+		const set2 = addValues(1, 10);
+
+		expect(set1.isSubsetOf(set2)).to.equal(true);
+		expect(set2.isSubsetOf(set1)).to.equal(true);
+	});
+
+	it('isSubsetOf between different sets', () => {
+		const set1 = addValues(1, 5);
+		const set2 = addValues(6, 10);
+
+		expect(set1.isSubsetOf(set2)).to.equal(false);
+		expect(set1.isSubsetOf(set2)).to.equal(false);
+	});
+
+	it('isSubsetOf between sets with common values', () => {
+		const set1 = addValues(1, 8);
+		const set2 = addValues(3, 10);
+
+		expect(set1.isSubsetOf(set2)).to.equal(false);
+		expect(set1.isSubsetOf(set2)).to.equal(false);
+
+		const set3 = addValues(1, 10);
+		const set4 = addValues(3, 8);
+
+		expect(set3.isSubsetOf(set4)).to.equal(false);
+		expect(set4.isSubsetOf(set3)).to.equal(true);
+	});
 });
